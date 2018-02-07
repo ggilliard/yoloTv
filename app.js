f2cba159aa468448acd50fb9948d7a44f783264d
Mei Li <mei.qli3@gmail.com>
2018-02-07 18:48:55 -0500

    fixing app.js and package.json

--- app.js
+++ app.js
@@ -66,31 +66,3 @@ app.get('/:series', function(req, res) {
 app.listen(3000, function() {
   console.log('server is listening to port 3000');
 });
-
-const app = express();
-const clientId = require('./api_key.js')
-
-app.use(logger('dev'));
-app.engine('handlebars', exphbs({defaultLayout: 'index'}));
-app.set('view engine', 'handlebars');
-app.use('/jquery', express.static(__dirname + '/node.modules/jquery/dist/'));
-
-
-function getData(APIKEY, LINEUPID) {
-    var url = `https://www.xmltvlistings.com/xmltv/get/${APIKEY}/${LINEUPID}`
-
-    return request({url:url},function(err,response, body) {
-        return response;
-    });
-}
-
-app.get('/', function (req, res) {
-    getData(clientId.clientId, 2961).then(function(data){
-        // console.log(data);
-    })
-    res.send(data)
-});
-           
-app.listen(3000)
-    
-    
