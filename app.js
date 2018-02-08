db1675fe6fce225784c3dbf4b440652ca1566b10
LadyG <ghgilliard@gmail.com>
2018-02-07 20:52:47 -0500

    more test data do not use

--- app.js
+++ app.js
@@ -13,15 +13,12 @@ const {
 const app = express();
 
 app.use(logger('dev'));
-app.engine('handlebars', exphbs({defaultLayout: 'index'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'index' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
-<<<<<<< HEAD
-
-=======
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
->>>>>>> 5c9ee2a18f4976a9e25813c389c90c00a5db6933
+
 
 var authOptions = {
   url: 'https://api.thetvdb.com/login',
@@ -84,23 +81,23 @@ function normalizeShows(data) {
   }
     
   
-app.get('/:series', function(req, res) {
-  const series = req.params.series;
-  let _jwt_token;
-
-  getJwtToken()
-    .then(function(token) {
-      _jwt_token = token;
-
-      return getShowSeries(_jwt_token, series)
-    })
-    .then(function(data) {
-      const showData = normalizeShows(data)
-      // console.log(showData)
-      return showData;
-    })
-  res.render('shows');
-})
+  app.get('/:series', function(req, res) {
+    const series = req.params.series;
+    let _jwt_token;
+  
+    getJwtToken()
+      .then(function(token) {
+        _jwt_token = token;
+  
+        return getShowSeries(_jwt_token, series)
+      })
+      .then(function(data) {
+        const showData = normalizeShows(data)
+  
+        // console.log(showData)
+        res.render('shows', showData);
+      })
+  });
 
 app.listen(3000, function() {
   console.log('server is listening to port 3000');
